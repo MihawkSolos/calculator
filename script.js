cd6b7f6f07e645c006355e43123036850579fa8f
Hakim Alharethi <hakimalharethi3@gmail.com>
2024-09-23 18:31:49 -0400

    Created three files, (html, css, js). Linked the css and js files to the html file and created the functions for the mathematical operations.

---
+++ script.js
@@ -0,0 +1,24 @@
+// add function
+function add (num1, num2){
+    return num1 + num2;
+}
+
+// subtract function
+function subtract (num1, num2){
+    return num1 - num2;
+}
+
+// multiply function
+function multiply (num1, num2){
+    return num1 * num2;
+}
+
+// divide function
+function divide (num1, num2){
+    return num1 / num2;
+}
+
+console.log(add(2,4));
+console.log(subtract(5,2));
+console.log(multiply(4,8));
+console.log(divide(30,5));
